ae02963d64d9be943094761c7fa0b9d5c04600a1
vobinhan <binhan091020@gmail.com>
2021-09-05 09:00:28 +0700

    update code

---
+++ main.js
@@ -0,0 +1,25 @@
+//music = ['cuoithoi'];
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+  var days = document.querySelector('.days span');
+  var hour = document.querySelector('.hour');
+  var min = document.querySelector('.min');
+  var second = document.querySelector('.second');
+
+  var startDate = new Date(2000, 9, 9);
+  days.innerText = Math.floor((new Date - startDate)/86400000);
+  countTime();
+
+  function countTime() {
+    let today = new Date();
+    let ms = (today - startDate) % 86400000;
+    hour.innerText = Math.floor(ms / 3600000);
+    min.innerText = Math.floor(ms % 3600000 / 60000);
+    second.innerText = Math.floor(ms % 3600000 % 60000 / 1000);
+  }
+  //document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
+
+  setInterval(countTime, 1000);
+
+}, false);    
